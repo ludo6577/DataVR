83c580c5e139433579a7dc1077fcf37abab09598
ludo6577 <ludo6577@gmail.com>
2017-02-07 03:14:08 +0100

    Indicator

---
+++ Assets/Scripts/Point.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Point : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        var text =  GetComponentInChildren<Text>();
+        text.text = string.Format("x: {0}\ny: {1}\nz: {2}", transform.position.x, transform.position.y, transform.position.z);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.transform.LookAt(Camera.main.transform); 
+	}
+}
